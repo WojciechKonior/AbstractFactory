e72cd98ad1ca203e8516b849a29685385ea3c9e6
WojciechKonior <78218606+WojciechKonior@users.noreply.github.com>
2022-08-30 11:52:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,101 +1,100 @@
 #include <iostream>
-class AbstractProductA {
- public:
-  virtual ~AbstractProductA(){};
-  virtual std::string UsefulFunctionA() const = 0;
+#include <cstring>
+
+class Door {
+public:
+    Door() {};
+    virtual ~Door() {}
+    virtual void Open() = 0;
 };
 
-class ConcreteProductA1 : public AbstractProductA {
- public:
-  std::string UsefulFunctionA() const override {
-    return "The result of the product A1.";
-  }
+class GasCarDoor : public Door {
+public:
+    GasCarDoor() { printf("Making a door for a gas car.\n"); }
+    void Open() { printf("Click\n"); }
 };
 
-class ConcreteProductA2 : public AbstractProductA {
-  std::string UsefulFunctionA() const override {
-    return "The result of the product A2.";
-  }
+class ElectricCarDoor : public Door {
+public:
+    ElectricCarDoor() { printf("Making a door for an electric car\n"); }
+    void Open() { printf("Shhhhh\n"); }
 };
 
-
-class AbstractProductB {
-
- public:
-  virtual ~AbstractProductB(){};
-  virtual std::string UsefulFunctionB() const = 0;
-  virtual std::string AnotherUsefulFunctionB(const AbstractProductA &collaborator) const = 0;
+class Engine {
+protected:
+    char _sound[15];
+public:
+    Engine() {strcpy(_sound, ""); }
+    virtual ~Engine() {}
+    virtual void Run() = 0;
 };
 
-class ConcreteProductB1 : public AbstractProductB {
- public:
-  std::string UsefulFunctionB() const override {
-    return "The result of the product B1.";
-  }
-
-  std::string AnotherUsefulFunctionB(const AbstractProductA &collaborator) const override {
-    const std::string result = collaborator.UsefulFunctionA();
-    return "The result of the B1 collaborating with ( " + result + " )";
-  }
+class GasEngine : public Engine {
+public:
+    GasEngine(){
+        strcpy(_sound, "vroom");
+        printf("Making a gas engine.\n");
+    }
+    void Run() { printf("%s\n", _sound); }
 };
 
-class ConcreteProductB2 : public AbstractProductB {
- public:
-  std::string UsefulFunctionB() const override {
-    return "The result of the product B2.";
-  }
-  std::string AnotherUsefulFunctionB(const AbstractProductA &collaborator) const override {
-    const std::string result = collaborator.UsefulFunctionA();
-    return "The result of the B2 collaborating with ( " + result + " )";
-  }
+class ElectricEngine : public Engine {
+public:
+    ElectricEngine(){
+        strcpy(_sound, "SHHHHH");
+        printf("Making an electric engine.\n");
+    }
+    void Run() { printf("%s\n", _sound); }
 };
 
-
-class AbstractFactory {
- public:
-  virtual AbstractProductA *CreateProductA() const = 0;
-  virtual AbstractProductB *CreateProductB() const = 0;
+class CarFactory {
+public:
+    virtual ~CarFactory() {}
+    virtual Door* BuildDoor() = 0;
+    virtual Engine* BuildEngine() = 0;
 };
 
-class ConcreteFactory1 : public AbstractFactory {
- public:
-  AbstractProductA *CreateProductA() const override {
-    return new ConcreteProductA1();
-  }
-  AbstractProductB *CreateProductB() const override {
-    return new ConcreteProductB1();
-  }
+class GasCarFactory : public CarFactory {
+public:
+    Door* BuildDoor(){
+        return new GasCarDoor();
+    }
+    Engine* BuildEngine(){
+        return new GasEngine();
+    }
 };
 
-class ConcreteFactory2 : public AbstractFactory {
- public:
-  AbstractProductA *CreateProductA() const override {
-    return new ConcreteProductA2();
-  }
-  AbstractProductB *CreateProductB() const override {
-    return new ConcreteProductB2();
-  }
+class ElectricCarFactory : public CarFactory {
+public:
+    Door* BuildDoor(){
+        return new ElectricCarDoor();
+    }
+    Engine* BuildEngine(){
+        return new ElectricEngine();
+    }
 };
 
+enum type_of_car{
+    gas_car = 1,
+    electric_car
+};
 
-void ClientCode(const AbstractFactory &factory) {
-  const AbstractProductA *product_a = factory.CreateProductA();
-  const AbstractProductB *product_b = factory.CreateProductB();
-  std::cout << product_b->UsefulFunctionB() << "\n";
-  std::cout << product_b->AnotherUsefulFunctionB(*product_a) << "\n";
-  delete product_a;
-  delete product_b;
+CarFactory* make_factory_for(char type_of_car){
+    switch(type_of_car){
+        case gas_car: return new GasCarFactory();
+        case electric_car: return new ElectricCarFactory();
+        default: return nullptr;
+    }
 }
 
-int main() {
-  std::cout << "Client: Testing client code with the first factory type:\n";
-  ConcreteFactory1 *f1 = new ConcreteFactory1();
-  ClientCode(*f1);
-  delete f1;
-  std::cout << std::endl;
-  std::cout << "Client: Testing the same client code with the second factory type:\n";
-  ConcreteFactory2 *f2 = new ConcreteFactory2();
-  ClientCode(*f2);
-  delete f2;
-  return 0;
+int main(){
+    auto CarPlant = make_factory_for(electric_car);
+    auto myDoor = CarPlant->BuildDoor();
+    auto myEngine = CarPlant->BuildEngine();
+    myDoor->Open();
+    myEngine->Run();
+    delete CarPlant;
+
+    printf("Done\n");
+    return 0;
 }
